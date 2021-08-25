ff2a6ad4f040e4befb507e33a9877a66e69a10b5
john horo <johnhoro125@gmail.com>
2021-08-25 09:49:39 +0530

    calculator completed

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//this function clear all the values
+function clearScreen(){
+    document.getElementById("result").value ="";
+}
+
+//this function display values
+function display(value){
+    document.getElementById("result").value += value;
+
+}
+//this function evaluate the expression and return result
+
+function calculate(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
